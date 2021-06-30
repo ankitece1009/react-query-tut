30bcf8e28f24a71f0e8adf15f7b2ddffd054ab19
Ankit <30548358+ankitece1009@users.noreply.github.com>
2021-06-30 13:34:41 +0530

    Update usersApi.js

--- src/usersApi.js
+++ src/usersApi.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const config = {
         headers:{
                 'Content-Type':'application/json',
-                'Authorisation':'<your_git token>'
+                'Authorisation':'<your api token>'
         }
 }
 const api = axios.create({
